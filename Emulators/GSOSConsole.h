4f472749df4b6c8bf7486958f5a2724f76b2829f
Kelvin Sherlock <ksherlock@gmail.com>
2016-07-11 02:27:21 +0000

    GS/OS Console Driver / ragel test.
    git-svn-id: svn://qnap.local/TwoTerm/trunk@3117 5590a31f-7b70-45f8-8c82-aa3a8e5f4507

---
+++ Emulators/GSOSConsole.h
@@ -0,0 +1,29 @@
+//
+//  GSOSConsole.h
+//  2Term
+//
+//  Created by Kelvin Sherlock on 7/9/2016.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import "Emulator.h"
+#include "iGeometry.h"
+#include "Screen.h"
+
+@interface GSOSConsole : NSObject <Emulator> {
+    unsigned cs;
+    
+    TextPort _textPort;
+    
+    std::vector<TextPort> _tpStack;
+    
+    unsigned _scratch[4];
+    
+    int _cursorType;
+    bool _consLF;
+    bool _consDLE;
+}
+
+@end
